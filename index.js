eb0abe36489deb1641b9cf823e7d22020735a767
Purvi kargathara <purvi.devmonk@gmail.com>
2023-12-25 15:08:11 +0530

    commit

--- index.js
+++ index.js
@@ -18,14 +18,53 @@ connectDb((e) => {
 });
 
 app.get("/", (req, res) => {
+  res.redirect("dashboard");
+});
+
+app.get("/login", (req, res) => {
   res.render("index");
+});
+
+app.post("/login", (req, res) => {
+  db.collection("Db")
+    .findOne({ username: req.body.username, password: req.body.password })
+    .then((data) => {
+      if (!data) res.render("index", { response: false });
+      else {
+        
+        res.redirect('dashboard')
+      }
+    })
+    .catch((e) => console.log(e));
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.post("/register", (req, res) => {
+  db.collection("Db")
+    .findOne({ username: req.body.username, password: req.body.password })
+    .then((data) => {
+      let exists = false
+      if (!data || data == null) {
+        db.collection("Db")
+        .insertOne(req.body);
+      }
+      else exists = true
+      res.render("register");
+    })
+    .catch((e) => console.log(e));
+});
+
+app.get("/dashboard", (req, res) => {
+  console.log(req.body)
+  res.render("dashboard");
 });
 
 app.post("/dashboard", (req, res) => {
   db.collection("Db")
-    .findOne({name: req.body.username, password: req.body.password})
-    .then((data) => res.render('dashboard', {response: data ? true : false}))
+    .findOne({ username: req.body.username, password: req.body.password })
+    .then((data) => res.render("dashboard", { response: data ? true : false }))
     .catch((e) => console.log(e));
 });
-
-app.listen(4000, () => console.log("listening2..."));
